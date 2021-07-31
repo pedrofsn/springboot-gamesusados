81603eaa8acd7d85bdc0cf7e099cac561f64870c
pedrofsn <pedrokra@gmail.com>
2021-07-31 11:42:24 -0300

    Creating extension functions

---
+++ src/main/kotlin/br/com/jogosusados/extensions/UserExtensions.kt
@@ -0,0 +1,18 @@
+package br.com.jogosusados.extensions
+
+import br.com.jogosusados.model.User
+import br.com.jogosusados.repository.UserRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+
+fun UserRepository.getUser(userDetails: UserDetails): User {
+    val email = userDetails.username
+    return findByEmail(email).get()
+}
+
+fun <T> UriComponentsBuilder.toResponseEntity(id : Long, body : T, path :String = "/{id}"): ResponseEntity<T> {
+    val uri: URI = path(path).buildAndExpand(id).toUri()
+    return ResponseEntity.created(uri).body(body)
+}
