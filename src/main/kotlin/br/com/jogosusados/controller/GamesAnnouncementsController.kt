2abcf865b75a29a7e18ac6e571ff98b504df017b
pedrofsn <pedrokra@gmail.com>
2021-07-31 11:43:50 -0300

    Adding endpoint to hande save/update announcement from a game. Remember, just 1 announcement by user per game.

--- src/main/kotlin/br/com/jogosusados/controller/GamesAnnouncementsController.kt
+++ src/main/kotlin/br/com/jogosusados/controller/GamesAnnouncementsController.kt
@@ -1,14 +1,20 @@
 package br.com.jogosusados.controller
 
+import br.com.jogosusados.extensions.getUser
+import br.com.jogosusados.extensions.toResponseEntity
 import br.com.jogosusados.model.GameAnnouncement
 import br.com.jogosusados.payload.GameAnnouncementDTO
 import br.com.jogosusados.payload.GameWithAnnouncementDTO
 import br.com.jogosusados.repository.GameAnnouncementRepository
+import br.com.jogosusados.repository.GameRepository
+import br.com.jogosusados.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
 import java.util.*
 
 @RestController
@@ -17,6 +23,12 @@ class GamesAnnouncementsController {
 
     @Autowired
     lateinit var gamesAnnouncementsRepository: GameAnnouncementRepository
+
+    @Autowired
+    lateinit var gameRepository: GameRepository
+
+    @Autowired
+    lateinit var usersRepository: UserRepository
 
     @GetMapping("/{id}")
     fun getDetail(@PathVariable id: Long): Optional<GameAnnouncementDTO> {
@@ -31,6 +43,24 @@ class GamesAnnouncementsController {
         val announcementsDTO = announcements.map { it.toAnnouncementDTO() }
         val response = GameWithAnnouncementDTO(game = gameDTO, announcements = announcementsDTO)
         return Optional.ofNullable(response)
+    }
+
+    @PostMapping("game/{idGame}/price/{price}")
+    fun saveAnnouncement(
+        @AuthenticationPrincipal userDetails: UserDetails,
+        @PathVariable idGame: Long,
+        @PathVariable price: Double,
+        uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<GameAnnouncementDTO> {
+        return gameRepository.findByIdOrNull(idGame)?.let { game ->
+
+            val user = usersRepository.getUser(userDetails)
+
+            val gameAnnouncent = GameAnnouncement(price = price, owner = user, game = game, id = 0)
+            val saved = gamesAnnouncementsRepository.save(gameAnnouncent)
+
+            return uriBuilder.toResponseEntity(saved.id, gameAnnouncent.toDTO())
+        } ?: ResponseEntity.notFound().build()
     }
 
 }
