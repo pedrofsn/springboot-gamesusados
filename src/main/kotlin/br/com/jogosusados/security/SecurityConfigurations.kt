7ced5848950b887f4b44b1ba9024f7ac5ff802a4
pedrofsn <pedrokra@gmail.com>
2022-07-21 23:34:12 -0300

    Allowing "POST platforms/register/{desired_name}" only to managers

--- src/main/kotlin/br/com/jogosusados/security/SecurityConfigurations.kt
+++ src/main/kotlin/br/com/jogosusados/security/SecurityConfigurations.kt
@@ -101,6 +101,7 @@ class SecurityConfigurations : WebSecurityConfigurerAdapter() {
         .authorizeRequests()
         .antMatchers(HttpMethod.GET, "/report").hasAuthority(Manager.authority)
         .antMatchers(HttpMethod.POST, "/games/platform/*/title/*").hasAuthority(Manager.authority)
+        .antMatchers(HttpMethod.POST, "/platforms/register/*").hasAuthority(Manager.authority)
         .antMatchers(HttpMethod.POST, "/announcements/*/toggle/*").hasAnyAuthority(Manager.authority, Regular.authority)
 
     private fun ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry.handleLoggedInEndpoints() = and()
