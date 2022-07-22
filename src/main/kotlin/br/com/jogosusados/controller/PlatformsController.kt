9d00d23921b5cf24f8f69a274569b8c525e0a664
pedrofsn <pedrokra@gmail.com>
2022-07-21 23:33:56 -0300

    Creating endpoint "POST platforms/register/{desired_name}" to save a new plataform (if doesn't exist yet)

--- src/main/kotlin/br/com/jogosusados/controller/PlatformsController.kt
+++ src/main/kotlin/br/com/jogosusados/controller/PlatformsController.kt
@@ -1,11 +1,15 @@
 package br.com.jogosusados.controller
 
+import br.com.jogosusados.error.PlatformFoundException
+import br.com.jogosusados.model.GamePlatform
+import br.com.jogosusados.payload.GamePlatformDTO
 import br.com.jogosusados.repository.GamePlatformRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("platforms")
@@ -17,5 +21,21 @@ class PlatformsController {
 
     @GetMapping
     fun getList() = gamesPlatforms.findAll().map { it.toDTO() }
+
+    @PostMapping("register/{title}")
+    fun saveGame(
+        @AuthenticationPrincipal userDetails: UserDetails,
+        @PathVariable title: String,
+        uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<GamePlatformDTO> {
+        val platformFound = gamesPlatforms.findGamePlatformByName(title)
+
+        if (platformFound == null) {
+            val saved = gamesPlatforms.save(GamePlatform(0, title))
+            return uriBuilder.toResponseEntity(saved.id, saved.toDTO())
+        }
+
+        throw PlatformFoundException()
+    }
 
 }
